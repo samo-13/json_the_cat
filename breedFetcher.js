d4770fdd80fef289716290f46ec6b2e969f1255c
Sarah Moss <sarah-moss@live.com>
2022-04-27 21:43:28 +0000

    Add request logic and find typeof body

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,23 @@
 const request = require('request'); // require the request library
 
 // The endpoint to allow us to search breed information is: 
-// https://api.thecatapi.com/v1/breeds/search.
+// https://api.thecatapi.com/v1/breeds/search --> returns []
+
+// https://api.thecatapi.com/v1/breeds/search?q=sib
+// --> returns
+// [{"weight":{"imperial":"8 - 16","metric":"4 - 7"},"id":"sibe","name":"Siberian","cfa_url":"http://cfa.org/Breeds/BreedsSthruT/Siberian.aspx","vetstreet_url":"http://www.vetstreet.com/cats/siberian","vcahospitals_url":"https://vcahospitals.com/know-your-pet/cat-breeds/siberian","temperament":"Curious, Intelligent, Loyal, Sweet, Agile, Playful, Affectionate","origin":"Russia","country_codes":"RU","country_code":"RU","description":"The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ","life_span":"12 - 15","indoor":0,"lap":1,"alt_names":"Moscow Semi-longhair, HairSiberian Forest Cat","adaptability":5,"affection_level":5,"child_friendly":4,"dog_friendly":5,"energy_level":5,"grooming":2,"health_issues":2,"intelligence":5,"shedding_level":3,"social_needs":4,"stranger_friendly":3,"vocalisation":1,"experimental":0,"hairless":0,"natural":1,"rare":0,"rex":0,"suppressed_tail":0,"short_legs":0,"wikipedia_url":"https://en.wikipedia.org/wiki/Siberian_(cat)","hypoallergenic":1,"reference_image_id":"3bkZAjRh1"}]
+
+// GET
+// /breeds/search
+// Search for a Breed by using part of it’s name as the ‘q’ query parameter.
+// e.g ?q=sib to search for Siberian
+request('https://api.thecatapi.com/v1/breeds/search ', (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+  console.log(typeof body)
+});
+
+
+
 
