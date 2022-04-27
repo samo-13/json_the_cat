ee3ac1e0340167e565d1f60d71d101f293ea4bfd
Sarah Moss <sarah-moss@live.com>
2022-04-27 21:47:57 +0000

    use JSON.parse to convert the JSON string into an actual object

--- breedFetcher.js
+++ breedFetcher.js
@@ -11,11 +11,16 @@ const request = require('request'); // require the request library
 // /breeds/search
 // Search for a Breed by using part of it’s name as the ‘q’ query parameter.
 // e.g ?q=sib to search for Siberian
-request('https://api.thecatapi.com/v1/breeds/search ', (error, response, body) => {
+request('https://api.thecatapi.com/v1/breeds/search?q=', (error, response, body) => {
+
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log(typeof body)
+  
+  console.log('body:', body); // Print the HTML for the page
+  console.log('typeof body:', typeof body)
+  const data = JSON.parse(body);
+  console.log('data:', data);
+  console.log('typeof data:', typeof data);
 });
 
 
